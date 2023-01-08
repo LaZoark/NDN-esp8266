07b4f8d3c775ee6531cd79c52671e52e3e0bc78a
LaZoark <emersonchen086@icloud.com>
2023-01-08 17:47:18 +0800

    decide the NDN-router

--- src/PingClient.cpp
+++ src/PingClient.cpp
@@ -8,30 +8,36 @@
 #include <esp8266ndn.h>
 #include "myconfig.h"
 
-const char* WIFI_SSID = __WIFI_SSID;
-const char* WIFI_PASS = __WIFI_PASSWORD;
+const char *WIFI_SSID = __WIFI_SSID;
+const char *WIFI_PASS = __WIFI_PASSWORD;
 
 // NDN ping-server, please refer the following links:
 // https://gerrit.named-data.net/plugins/gitiles/ndn-tools/+/00aa181b4951fd5d9224e934ae08c95b1e167d37/tools/ping/README.md
 // https://www.lists.cs.ucla.edu/pipermail/nfd-dev/2014-August/000361.html
-// 
-const char* PREFIX0 = "/ndn/edu/arizona/ping";   
-const char* PREFIX1 = "/ndn/edu/memphis/ping";   
-const char* PREFIX2 = "/ndn/edu/ucla/ping";     // UCLA
-// const char* PREFIX0 = "/ndn/edu/nycu/ping";   // 陽交
-// const char* PREFIX2 = "/ndn/edu/nthu/ping";   // 清華
+//
+// const char* PREFIX0 = "/ndn/edu/arizona/ping";
+// const char* PREFIX1 = "/ndn/edu/memphis/ping";
+const char *PREFIX0 = "/ndn/edu/nycu/ether/310505030/ping"; // 陽交 + studientID
+const char *PREFIX1 = "/ndn/edu/nycu/udp/ping";             // 陽交
+const char *PREFIX2 = "/ndn/edu/ucla/ping";                 // UCLA
+const char *PREFIX3 = "/ndn/edu/nthu/udpm/ping";            // 清華
 
 esp8266ndn::UdpTransport transport;
+// ndnph::Face face(transport);
 ndnph::Face face(transport);
 
 ndnph::StaticRegion<1024> region;
 ndnph::PingClient client0(ndnph::Name::parse(region, PREFIX0), face);
 ndnph::PingClient client1(ndnph::Name::parse(region, PREFIX1), face);
 ndnph::PingClient client2(ndnph::Name::parse(region, PREFIX2), face);
+ndnph::PingClient client3(ndnph::Name::parse(region, PREFIX3), face);
 
-void
-setup()
+void setup()
 {
+  // Serial.print('transport: ');
+  // Serial.println();
+  // Serial.print('face: ');
+  // Serial.println(face);
   Serial.begin(115200);
   Serial.println();
   esp8266ndn::setLogOutput(Serial);
@@ -40,7 +46,8 @@ setup()
   WiFi.mode(WIFI_STA);
   WiFi.setSleep(false);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+  if (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
     Serial.println(F("WiFi connect failed"));
     ESP.restart();
   }
@@ -53,15 +60,29 @@ setup()
   WiFiClientSecure fchSocketClient;
   fchSocketClient.setInsecure();
 #endif
-  auto fchResponse = esp8266ndn::fchQuery(fchSocketClient);
-  if (!fchResponse.ok) {
+  // auto fchResponse = esp8266ndn::fchQuery(fchSocketClient);
+  // auto fchResponse = esp8266ndn::fchQuery(fchSocketClient, "http://hobo.cs.arizona.edu/"); // NG
+  // auto fchResponse = esp8266ndn::fchQuery(fchSocketClient, "https://suns.cs.ucla.edu/");
+  // auto fchResponse = esp8266ndn::fchQuery(fchSocketClient, "http://ndn-fch.named-data.net/"); // NG
+  // auto fchResponse = esp8266ndn::fchQuery(fchSocketClient, "https://fch.ndn.today/"); // 會幫忙找到最佳的 NDN-Router
+  // if (!fchResponse.ok)
+  // {
+  //   ESP.restart();
+  // }
+  // transport.beginTunnel(fchResponse.ip);
+  
+  const char* NDN_ROUTER_HOST = "titan.cs.memphis.edu";
+  uint16_t _remotePort = 6363;
+  uint16_t _localPort = 6363;
+  IPAddress routerIp;
+  if (!WiFi.hostByName(NDN_ROUTER_HOST, routerIp)) {
+    Serial.println("cannot resolve router IP");
     ESP.restart();
   }
-  transport.beginTunnel(fchResponse.ip);
+  transport.beginTunnel(routerIp, _remotePort, _localPort);
 }
 
-void
-printCounters(const char* prefix, const ndnph::PingClient& client)
+void printCounters(const char *prefix, const ndnph::PingClient &client)
 {
   auto cnt = client.readCounters();
   Serial.printf("%8dI %8dD %3.3f%% %s\n", static_cast<int>(cnt.nTxInterests),
@@ -71,13 +92,23 @@ printCounters(const char* prefix, const ndnph::PingClient& client)
 void loop()
 {
   face.loop();
-  delay(1);
+  delay(3);
 
   static uint16_t i = 0;
-  if (++i % 1024 == 0) {
+  if (++i % 1024 == 0)
+  {
     printCounters(PREFIX0, client0);
     printCounters(PREFIX1, client1);
     printCounters(PREFIX2, client2);
-    Serial.println(F("-----------------------------"));
+    printCounters(PREFIX3, client3);
+    Serial.println(F("---------------------------------"));
   }
 }
+
+
+// 9264 [AutoConfig] https://fch.ndn.today/ body: ndn.ist.osaka-u.ac.jp
+// 9288 [AutoConfig] DNS resolved to: 133.1.17.51
+// 9289 [UdpTransport] connecting to 133.1.17.51:6363 from :6363
+//        4I        0D 0.000% /ndn/edu/nycu/ether/310505030/ping
+//        4I        0D 0.000% /ndn/edu/nycu/udp/ping
+//        4I        0D 0.000% /ndn/edu/nthu/udpm/ping
